e5e6fd314687a4249da1403310b2a57bc4d1990d
Brayan valderrama <rubianostivenbrayan13@gmail.com>
2026-05-14 11:46:52 -0500

    plantilla 3

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,5 @@
+#suma 
 
-n1 = int(input("Digite el primer número: "))
-n2 = int(input("Digite el segundo número: "))
+#resta
 
-
-print("\n--- CALCULADORA ---")
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicación")
-print("4. División")
+#multiplicacion 
