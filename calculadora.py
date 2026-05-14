6b5f88680b6744b6690f37f309dc6e1f7d4afdce
Brayan valderrama <rubianostivenbrayan13@gmail.com>
2026-05-14 11:42:17 -0500

    calculadora2

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+
+n1 = int(input("Digite el primer número: "))
+n2 = int(input("Digite el segundo número: "))
+
+
+print("\n--- CALCULADORA ---")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
